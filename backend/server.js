45a7d73753bebb6953397ee23cc262d9effd241a
A Shendage <shendageanand7@gmail.com>
2024-02-23 16:33:45 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const exp = require('express');
+const {chats} = require('./data/data.js')
+const app = exp();
+
+app.listen('9007')
+app.get('/', (req, res)=>{
+    res.send('Welcome to Node Framework called Express')
+})
+
+app.get('/api/chats', (req, res)=>{
+    res.send(chats)
+})
+
+app.get('/api/chats/:id', (req, res)=>{
+    const fetchedChat = chats.find((x)=> x._id === req.params.id)
+    res.send(fetchedChat);
+})
+
